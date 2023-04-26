fb992eb27cffeca3cc208a2dbd31074e3887394a
mwektaehtabr <mwektaehtabr@gmail.com>
2023-04-26 21:31:22 +0900

    //

--- VERSES/4.php
+++ VERSES/4.php
@@ -1,1 +1,1 @@
-and later that year the child was born, in the cab of a pickup truck in a traffic jam on the highway on the rainy autumn morning of November 5, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
+and later that year the child was born, in the cab of a pickup truck in a traffic jam on the highway on the rainy autumn morning of November 6, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
