6d23593ca2196f36665503d77f9e4088c484d354
mwektaehtabr <mwektaehtabr@gmail.com>
2023-04-27 20:12:32 +0900

    //

---
+++ VERSES/The-One-About-The-Folk.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php include "comment.php";?>
+<html>
+  <head>
+    <title>The One About The Folk</title>
+    <?php include "meta.php";?>
+  </head>
+  <body>
+    <?php include "header.php";?>
+        <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the sunny autumn morning of November 6, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
+        <p>When he was two his mother died of influenza and when he was three his father applied for a job as a federal agent and when he was four his father married a pharmacist and a sister was born during a rainstorm and dandelions bloomed and milkweed flowered and a tornado destroyed the library downtown and when he was sixteen he found a guitar in a dumpster and with money from a job at a diner he bought some strings for the guitar, twisting the pegs on the guitar to tighten the strings by the light of a lamp in a bedroom with walls painted pink.</p>
+        <p>His father begged him to go to college, so he applied, and he enrolled, but he only attended for a week before dropping out to record an album.</p>
+        <p>&ldquo;I&rsquo;m sorry,&rdquo; Will said.</p>
+        <p>His father was too distressed to speak.</p>
+        <p>&ldquo;I just,&rdquo; Will said.</p>
+        <p>A breeze blew through the leaves of the beech trees beyond the windows.</p>
+        <p>&ldquo;I need to do this,&rdquo; Will said.</p>
+        <p>He recorded the album. Landed a record deal with an indie label. His father bought a hundred copies, but otherwise in that first year the album hardly sold. The critical reviews were rave, hailing the album as a new genre of folk. Quarkpunk. He was broke. He was determined. He lived in a van, touring to promote the album. Louisville. Nashville. Memphis. Birmingham. Atlanta. Charlotte. Savannah. Charleston. Jacksonville. Houston. Dallas. Austin. Showering in gyms. Washing clothing in sinks. Drying clothing on hangers. Surviving on bread, apples, cheddar, peanut butter spooned straight from the jar. Drinking water from a canteen. Coffee from a styrofoam cup. Bourbon from a plastic mug. Sewing ripped clothing with tattooed hands. Lonely. Working on songs for a new album between shows.</p>
+        <p>One night after a show he hooked up with a fan.</p>
+        <p>&ldquo;You&rsquo;re gonna be famous someday,&rdquo; Roxanne said.</p>
+        <p>Her eyeshadow was turquoise.</p>
+        <p>&ldquo;I&rsquo;m being real right now,&rdquo; Roxanne said.</p>
+        <p>As she drifted off to sleep, lying there naked beside him in the flickering light of a candle, he suddenly thought of some lyrics for a verse. He realized he needed to start driving. He wrote her a note. He got dressed. Baggy flannel. Skinny jeans. Combat boots. He drank a sip of water from the faucet. Then he slipped out the door. Lonely as ever. The air was humid. Still thinking about the new verse, he walked back toward the club where the van was parked. Chirping frogs. Warbling crickets. Power lines humming above the street. Moonlight shimmered on shards of glass on the sidewalk. Approaching an intersection, he heard footsteps as a stranger ran at him from an alley, stabbing him in the chest, slashing him in the stomach, and then shoving him down onto the pavement, grabbing for a wallet. Cursing at empty pockets. Then the stranger was gone. Will bled to death in a gutter, gasping, breathing, too shocked to cry.</p>
+        <?php include "randomize.php";?>
+      <?php include "footer.php";?>
+  </body>
+</html>
