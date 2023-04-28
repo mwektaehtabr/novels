6f0b02c55b89aaa93984a2a0e1a8b75f8956b257
mwektaehtabr <mwektaehtabr@gmail.com>
2023-04-28 16:14:46 +0900

    //

---
+++ VERSES/The-One-About-The-Rock.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php include "comment.php";?>
+<html>
+  <head>
+    <title>The One About The Rock</title>
+    <?php include "meta.php";?>
+  </head>
+  <body>
+    <?php include "header.php";?>
+        <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the foggy autumn evening of November 3, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
+        <p>Twenty-nine years later he sat on the balcony of the penthouse suite of a historic hotel. Buzzcut. Noseringed. Tattooed. Founder of a new genre of rock. Quantumwave. Dressed in a black kimono. Beside the copper bowl of yuzus on the table, lyrics were written into the pages of a notebook. Verse for a new song. As he drifted off into a dream, the pen dropped from his hands. The doorbell chimed. The telephone rang. A pigeon fluttered onto the balcony, cooed, and then flapped away. Even when the rain began to drizzle, he still didn&rsquo;t wake. He died there on the balcony of an overdose. Valium, heroin, xanax, cocaine. By the time that the body was discovered, the notebook had been destroyed by the rain.</p>
+        <?php include "randomize.php";?>
+      <?php include "footer.php";?>
+  </body>
+</html>
