4ee3d962069770f6b03f0ea3eec303bc86b79bcf
mwektaehtabr <mwektaehtabr@gmail.com>
2023-05-02 15:32:23 +0900

    //

--- VERSES/xyzzy.php
+++ VERSES/xyzzy.php
@@ -13,14 +13,17 @@
     <?php include "header.php";?>
         <p class="c">CONTENTS</p>
         <p><a href="The-One-About-The-Drifter">The One About The Drifter</a></p>
+        <p><a href="The-One-About-The-Folk">The One About The Folk</a></p>
         <p><a href="The-One-About-The-Jazz">The One About The Jazz</a></p>
         <p><a href="The-One-About-The-Nomad">The One About The Nomad</a></p>
         <p><a href="The-One-About-The-Pop">The One About The Pop</a></p>
+        <p><a href="The-One-About-The-Rap">The One About The Rap</a></p>
         <p><a href="The-One-About-The-Rock">The One About The Rock</a></p>
         <p><a href="The-One-About-The-Song">The One About The Song</a></p>
         <p><a href="The-One-About-The-Sonogram">The One About The Sonogram</a></p>
         <p><a href="The-One-About-The-Star">The One About The Star</a></p>
         <p><a href="The-One-About-The-Traveler">The One About The Traveler</a></p>
+        <p><a href="The-One-About-The-Variations">The One About The Variations</a></p>
         <p><a href="The-One-About-The-Wanderer">The One About The Wanderer</a></p>
       <?php include "footer.php";?>
   </body>
