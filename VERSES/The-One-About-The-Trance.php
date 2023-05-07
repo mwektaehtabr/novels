0607df7a577ed0fe03e586dff69bd98f559aa9fa
mwektaehtabr <mwektaehtabr@gmail.com>
2023-05-07 11:13:58 -0400

    //

--- VERSES/The-One-About-The-Trance.php
+++ VERSES/The-One-About-The-Trance.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <?php include "header.php";?>
-        <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the rainy autumn evening of November 14, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
+        <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the sunny autumn afternoon of November 16, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
         <p>Thirty-five years later he sat at a mixer in the production room in a studio. Bleachhaired. Spectacled. Perfumed. Founder of a new genre of trance. Quarkcore. Dressed in some black sweats. Steam rising from a mug of sencha. Foil shimmering in a box of truffles. Prepping to record another verse, he became aware that he suddenly felt drowsy. As he drifted off into a stupor, the headphones slipped from his ears. He died there at the mixer of asphyxiation. Gas leak. Carbon monoxide. Malfunction in the detector.</p>
         <?php include "randomize.php";?>
       <?php include "footer.php";?>
