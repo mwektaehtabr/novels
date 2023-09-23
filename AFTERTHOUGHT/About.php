6b245459a2d8fd638989fcac03823b0290005b4c
mwektaehtabr <mwektaehtabr@gmail.com>
2023-09-23 17:17:55 -0400

    //

--- AFTERTHOUGHT/About.php
+++ AFTERTHOUGHT/About.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <?php include "header.php";?>
-        <p>AFTERTHOUGHT is a serialized novel by <a href="https://www.mwektaehtabr.com">mwektaehtabr</a>. Intentionally posthumous, the novel <?php include "status.php";?> dead.</p>
+        <p>AFTERTHOUGHT is a serialized novel by <a href="https://www.mwektaehtabr.com">mwektaehtabr</a>. Intentionally posthumous, the novel will be published only after the author is dead.</p>
         <p>mwektaehtabr is also the author of the interlinked novel <a href="https://www.untold.pub">UNTOLD</a> and the randomized novel <a href="https://www.verses.pub">VERSES</a>.</p>
         <p>AFTERTHOUGHT is published by <a href="https://virtualpress.net">Virtual Press</a>.</p>
       <?php include "footer.php";?>
