65967768063e0f1dea23220eda94cbf64f40c19e
mwektaehtabr <mwektaehtabr@gmail.com>
2023-11-23 05:06:37 -0500

    //

--- UNTOLD/meta.php
+++ UNTOLD/meta.php
@@ -2,3 +2,4 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="virtual.css">
+    <link rel="stylesheet" type="text/css" href="tmp.css">
--- VERSES/meta.php
+++ VERSES/meta.php
@@ -2,4 +2,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="virtual.css">
+    <link rel="stylesheet" type="text/css" href="tmp.css">
     <script src="randomize.js"></script>
