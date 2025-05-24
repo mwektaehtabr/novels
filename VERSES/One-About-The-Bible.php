eb7c532f59a602bd7c991bca037e31591e86ed1e
mwektaehtabr <mwektaehtabr@gmail.com>
2025-05-24 09:12:52 -0400

    //

--- VERSES/One-About-The-Bible.php
+++ VERSES/One-About-The-Bible.php
@@ -8,7 +8,7 @@
   <body>
     <?php include "header.php";?>
       <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the misty autumn morning of November 6, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
-      <p>Sixty-seven years later he sat in the sunlight by a grassy river. Bald. Wrinkled. Dressed in a sapphire habit and some canvas sneakers. Meditating on a verse from the bible. A parakeet chirped nearby. A stork flapped past. Across the water, a bell was pealing in the chapel. After all of those years, still searching for the truth about reality. He died there where he sat.</p>
+      <p>Sixty-seven years later he sat in the sunlight by a grassy river. Bald. Wrinkled. Dressed in a sapphire habit and some canvas sneakers. Meditating on a verse from the Bible. A parakeet chirped nearby. A stork flapped past. Across the water, a bell was pealing in the chapel. After all of those years, still searching for the truth about reality. He died there where he sat.</p>
       <?php include "randomize.php";?>
     <?php include "footer.php";?>
   </body>
--- VERSES/One-About-The-Sutras.php
+++ VERSES/One-About-The-Sutras.php
@@ -8,7 +8,7 @@
   <body>
     <?php include "header.php";?>
       <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the windy autumn morning of November 6, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
-      <p>Eighty-nine years later he sat in the sunlight by a mossy river. Bald. Wrinkled. Dressed in a crimson kasaya and some cork sandals. Meditating on a verse from the sutras. A parakeet chirped nearby. A myna fluttered past. Across the water, a gong was clanging in the vihara. After all of those years, still searching for the truth about reality. He died there where he sat.</p>
+      <p>Eighty-nine years later he sat in the sunlight by a mossy river. Bald. Wrinkled. Dressed in a crimson kasaya and some cork sandals. Meditating on a verse from the Sutras. A parakeet chirped nearby. A myna fluttered past. Across the water, a gong was clanging in the vihara. After all of those years, still searching for the truth about reality. He died there where he sat.</p>
       <?php include "randomize.php";?>
     <?php include "footer.php";?>
   </body>
