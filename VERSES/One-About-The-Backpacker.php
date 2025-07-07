d0ba03179d4fe76cf7ea592bf2d3f5f8898bc21f
mwektaehtabr <mwektaehtabr@gmail.com>
2025-07-07 10:54:43 -0400

    //

---
+++ VERSES/One-About-The-Backpacker.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php include "comment.php";?>
+<html dir="ltr" lang="en">
+  <head>
+    <title>One About The Backpacker</title>
+    <?php include "meta.php";?>
+  </head>
+  <body>
+    <?php include "header.php";?>
+      <?php include "0.php";?> <?php include "1.php";?>
+      <?php include "2.php";?> <?php include "3.php";?>
+      <p>He flew back across the ocean with her the next morning. Bought a house in the woods. Worked for fifty-seven years as a carpenter. Had two kids and three grandkids and a reputation for always smiling. Lived through a random stabbing at a post office, a catastrophic blizzard, a cataclysmic flood, a fiery car crash on an icy highway, a hundred-story drop in a malfunctioning elevator, a weekslong power outage during a heat wave, a cholera epidemic, the death of both parents, cardiac arrest in the parking lot of a shopping mall, surgery for a brain tumor, chemotherapy, widowhood, bronchitis, mass suicides aboard the space station, islands vanishing under the rising sea, a world at war over water. Crouched over a cookstove in the ruins of an abandoned library, buttoned into a tattered jacket, stirring a pot of oats with a plastic spoon. Heard the whoosh of a rocket, then an explosion. Lay gasping among the rubble, looking up at the stars. Died happy.</p>
+      <?php include "randomize.php";?>
+    <?php include "footer.php";?>
+  </body>
+</html>
---
+++ VERSES/One-About-The-Searcher.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php include "comment.php";?>
+<html dir="ltr" lang="en">
+  <head>
+    <title>One About The </title>
+    <?php include "meta.php";?>
+  </head>
+  <body>
+    <?php include "header.php";?>
+      <?php include "0.php";?> <?php include "1.php";?>
+      <?php include "2.php";?> <?php include "3.php";?>
+      
+      <?php include "randomize.php";?>
+    <?php include "footer.php";?>
+  </body>
+</html>
