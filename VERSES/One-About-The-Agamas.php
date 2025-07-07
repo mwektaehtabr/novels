5c1f55f1bcf6109120a365b36bc6034a355bde58
mwektaehtabr <mwektaehtabr@gmail.com>
2025-07-07 11:24:35 -0400

    //

--- VERSES/One-About-The-Agamas.php
+++ VERSES/One-About-The-Agamas.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <?php include "header.php";?>
-      <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the frosty autumn morning of November 6, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
+      <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the rainy autumn morning of November 6, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
       <p>Seventy-nine years later he sat in the sunlight by a grassy stream. Bald. Wrinkled. Dressed in some plastic glasses and a yellow lungi. Meditating on a verse from the Agamas. A parakeet chirped nearby. A magpie soared past. Across the water, a tambourine was jingling in the basadi. After all of those years, still searching for the truth about reality. He died there where he sat.</p>
       <?php include "randomize.php";?>
     <?php include "footer.php";?>
