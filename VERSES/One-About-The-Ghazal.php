172ffe39b24e034ff59c026a1b631c6446a142e4
mwektaehtabr <mwektaehtabr@gmail.com>
2025-07-26 09:32:03 -0400

    //

--- VERSES/One-About-The-Ghazal.php
+++ VERSES/One-About-The-Ghazal.php
@@ -8,7 +8,7 @@
   <body>
     <?php include "header.php";?>
       <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the rainy autumn afternoon of November 3, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
-      <p>At the age of thirty-nine he sat on a bench in the gardens of a chateau, wearing an emerald poncho and muddy sneakers, writing a verse of poetry in a notebook. A ghazal for a new collection. Dark clouds were floating through the sky above the chateau. Cicadas were buzzing in the juniper trees. He watched as a hummingbird flitted past the bench. He could smell the scent of barbecue on the breeze. Thunder rumbled. Glancing back down at the notebook, he pressed the tip of the pencil to the paper, about to write another word, and then he suddenly became aware of a tingling sensation, and a moment later the lightning struck. His body was eventually discovered, but the poem was never found.</p>
+      <p>Thirty-nine years later he sat on a bench in the gardens of a chateau, wearing an emerald poncho and muddy sneakers, writing a verse of poetry in a notebook. A ghazal for a new collection. Dark clouds were floating through the sky above the chateau. Cicadas were buzzing in the juniper trees. He watched as a hummingbird flitted past the bench. He could smell the scent of barbecue on the breeze. Thunder rumbled. Glancing back down at the notebook, he pressed the tip of the pencil to the paper, about to write another word, and then he suddenly became aware of a tingling sensation, and a moment later the lightning struck. His body was eventually discovered, but the poem was never found.</p>
     <?php include "footer.php";?>
   </body>
 </html>
