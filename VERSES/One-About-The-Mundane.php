02805132929b7e6c719dde50c2f475dbea5c825f
mwektaehtabr <mwektaehtabr@gmail.com>
2025-08-10 17:11:02 -0400

    //

--- VERSES/One-About-The-Mundane.php
+++ VERSES/One-About-The-Mundane.php
@@ -8,7 +8,7 @@
   <body>
     <?php include "header.php";?>
       <?php include "0.php";?> and later that year the child was born, in a busy urban hospital on the windy autumn morning of November 11, 1985. If he&rsquo;d been a girl, his parents would have named him Megan, but instead his parents named him William.</p>
-      <p>He lived for ninety-two years, but he did nothing particularly interesting during that time. In the end, as he lay dying in a nondescript bedroom with generic wallpaper and bland carpeting, he was somewhat embarrassed how boring of a life he&rsquo;d lived. When he died even his death was uneventful.</p>
+      <p>He lived for ninety-two years, but nothing particularly interesting happened to him during that time. In the end, as he lay dying in a nondescript bedroom with generic wallpaper and bland carpeting, he was somewhat embarrassed by how boring of a life he&rsquo;d lived. When he died even his death was uneventful.</p>
     <?php include "footer.php";?>
   </body>
 </html>
